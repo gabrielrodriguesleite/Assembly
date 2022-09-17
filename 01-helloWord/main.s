ab5202c451ed3c9d69b065ab184993536faed23a
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-16 23:43:33 -0300

    [doc] section .text -- holds the executable code

--- 01-helloWord/main.s
+++ 01-helloWord/main.s
@@ -1,16 +1,16 @@
-.global _start /*_start é obrigatório como o main já que é procurado pelo linker*/
-.text
+.global _start					/* _start é obrigatório como o main já que é procurado pelo linker*/
+.text							/* ou "section .text" -- o código executavel vai na seção .text*/
 _start:
-  movq $1,  %rax
-  movq $1,  %rdi
-  leaq msg(%rip), %rsi
-  movq $13, %rdx
-  syscall
-  movq $60, %rax
-  xorq %rdi, %rdi
-  syscall
-.section .rodata
+	movq $1,  %rax
+	movq $1,  %rdi
+	leaq msg(%rip), %rsi
+	movq $13, %rdx
+	syscall
+	movq $60, %rax
+	xorq %rdi, %rdi
+	syscall
+	.section .rodata
 msg:
-  .ascii "Olá mundo!\n"
+	.ascii "Olá mundo!\n"	
 
 /*Programa usando sintaxe AT & T*/
